625446ea4a1692d8fec344b7e132a4ca392908c5
Nguyen Nhat Nam <121727434+Ronemdo@users.noreply.github.com>
2025-01-04 21:25:04 +0700

    Add files via upload

---
+++ Bai 2 Lap trinh voi T-sql.sql
@@ -0,0 +1,76 @@
+﻿--2.1
+BEGIN
+	DECLARE @a int = 5;
+	DECLARE @b int = 4;
+    DECLARE @cong int;
+	DECLARE @tru int;
+	DECLARE @nhan int;
+	DECLARE @chia int;
+	PRINT N'Số a =' + CAST(@a as varchar(15));
+	PRINT N'Số b =' + CAST(@b as varchar(15));
+	SET @cong = @a + @b;
+	SET @tru = @a - @b;
+	SET @nhan = @a * @b;
+	SET @chia = @a / @b;
+	PRINT N'Cộng =' + CAST(@cong as varchar(15));
+	PRINT N'Trừ =' + CAST(@tru as varchar(15));
+	PRINT N'Nhân =' + CAST(@nhan as varchar(15));
+	PRINT N'Chia =' + CAST(@chia as varchar(15));
+
+END;
+--2.2
+BEGIN 
+	DECLARE @thanghientai int;
+	DECLARE @quyhientai int;
+
+	SET @thanghientai = MONTH(GETDATE())
+
+	IF @quyhientai >= 1 and @quyhientai <=3
+		 SET @quyhientai = 1;
+	ELSE IF @quyhientai >= 4 and @quyhientai <=6
+		 SET @quyhientai = 2;
+	ELSE IF @quyhientai >= 7 and @quyhientai <=9
+		 SET @quyhientai = 3;
+	ELSE 
+		 SET @quyhientai =4;
+
+	PRINT N'Tháng hiện tại: ' + CAST(@thanghientai as varchar(15));
+	PRINT N'Quý hiện tại: ' + CAST(@quyhientai as varchar(15));
+END;
+--2.3
+BEGIN
+	DECLARE @n int = 5;
+	DECLARE @S1 int = 0;
+	DECLARE @i int = 1;
+	DECLARE @S2 float = 0;
+
+	WHILE (@i <= @n)
+		BEGIN
+			SET @s1 = @S1 + @i;
+			SET @s2 = @S2 + 1/@i;
+			SET @i = @i +1;
+		END;
+	PRINT N'Tổng của S1 là: ' + CAST(@S1 as varchar(15));
+	PRINT N'Tổng của S2 là: ' + CAST(@S2 as varchar(15));
+END;
+--2.4
+BEGIN
+	DECLARE @noisinh nvarchar(250) = N'TT Huế';
+	DECLARE @masinhvien nvarchar(10);
+	DECLARE @hodem nvarchar(45);
+	DECLARE @ten nvarchar(15);
+	DECLARE @ngaysinh date;
+
+	SELECT 
+		@masinhvien = s.masinhvien,
+		@hodem = s.hodem,
+		@ten = s.ten,
+		@ngaysinh = s.ngaysinh
+	FROM SINHVIEN s
+	WHERE s.noisinh = @noisinh;
+
+	PRINT N'Mã sinh viên: ' + @masinhvien;
+	PRINT N'Họ đệm: ' + @hodem;
+	PRINT N'Tên: ' + @ten;
+	PRINT N'Ngày sinh: ' + CAST(@ngaysinh as varchar(15));
+END;
---
+++ Bai1 Tong quan ve co so du lieu.sql
@@ -0,0 +1,94 @@
+﻿--1.1
+SELECT *
+FROM SINHVIEN
+WHERE hodem like N'Lê%'
+--1.2
+SELECT *
+FROM SINHVIEN
+WHERE hodem like N'Thị%' or hodem like N'%Thị%'
+--1.3
+SELECT *
+FROM SINHVIEN
+WHERE hodem like N'% Văn'
+--1.4
+SELECT s.masinhvien, s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh, s.gioitinh,l.malop,l.tenlop
+FROM SINHVIEN as s JOIN LOP as l ON s.malop = l.malop
+WHERE s.hodem like N'Dư %' or s.hodem like N'Dư' or s.ten like N'V%'
+--1.5
+SELECT masinhvien,hodem+' '+ten as N'Họ và tên',ngaysinh,gioitinh,noisinh
+FROM SINHVIEN
+WHERE noisinh like N'%Huế%'
+--1.6
+SELECT s.masinhvien, s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh, s.gioitinh,l.malop,l.tenlop
+FROM SINHVIEN as s JOIN LOP as l ON s.malop = l.malop
+WHERE YEAR(s.ngaysinh) = '1992' and MONTH(s.ngaysinh) between 3 and 8
+--1.7
+SELECT s.masinhvien, s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh, s.gioitinh,l.malop,l.tenlop
+FROM SINHVIEN as s JOIN LOP as l ON s.malop = l.malop
+WHERE s.gioitinh = 0 and MONTH(s.ngaysinh) between 5 and 11
+--1.8
+SELECT s.masinhvien,s.hodem+' '+ s.ten as N'Họ và tên' ,s.ngaysinh,s.gioitinh,l.malop,l.tenlop 
+from SINHVIEN  s join LOP  l on s.malop = l.malop
+where s.hodem NOT LIKE N'Lê%' and s.hodem NOT LIKE N'Dư%' and s.hodem NOT LIKE N'Võ%' and s.hodem NOT LIKE N'Nguyễn%';
+--1.9 (thiếu 1)
+SELECT s.masinhvien, s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh, s.gioitinh,l.malop,l.tenlop
+FROM SINHVIEN  s JOIN LOP  l ON s.malop = l.malop
+WHERE s.hodem like N'Lê' and s.ten like N'Nga' or s.ten like N'Lý'
+--1.10
+SELECT masinhvien,hodem+' '+ten as N'Họ và tên',ngaysinh,gioitinh,noisinh
+FROM SINHVIEN
+WHERE noisinh is NULL
+--1.11
+SELECT l.malop,l.tenlop,COUNT(s.masinhvien) as N'Tổng số sinh viên'
+FROM LOP l left join SINHVIEN s on l.malop = s.malop
+GROUP BY l.malop,tenlop
+--1.12
+SELECT l.malop,l.tenlop
+FROM LOP l left join SINHVIEN s on l.malop = s.malop
+WHERE s.malop is NULL
+--1.13
+SELECT TOP 1 l.malop,l.tenlop,COUNT(s.masinhvien) as N'Tổng số sinh viên'
+FROM LOP l left join SINHVIEN s on l.malop = s.malop
+GROUP BY l.malop,tenlop
+ORDER BY N'Tổng số sinh viên' DESC
+--1.14
+SELECT TOP 5 noisinh, COUNT(masinhvien) as TongSV
+FROM SINHVIEN
+GROUP BY noisinh
+ORDER BY TongSV DESC
+--1.15
+SELECT TOP 5 s.masinhvien,s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh,s.gioitinh,l.malop,l.tenlop,(diemmon1+diemmon2+diemmon3)/3.0 as N'Điểm trung bình'
+FROM SINHVIEN s JOIN DIEMTS d on s.masinhvien = d.masinhvien JOIN LOP l on l.malop = s.malop
+ORDER BY N'Điểm trung bình' DESC,gioitinh ASC
+--1.16
+SELECT TOP 5 s.masinhvien,s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh,s.gioitinh,d.diemmon1
+FROM SINHVIEN s JOIN DIEMTS d ON s.masinhvien = d.masinhvien
+ORDER BY diemmon1 DESC
+--1.17
+SELECT TOP 5 s.masinhvien,s.hodem+' '+s.ten as N'Họ và tên',s.ngaysinh,s.gioitinh,(diemmon1+diemmon2+diemmon3)/3.0 as N'Điểm trung bình'
+FROM SINHVIEN s JOIN DIEMTS d on s.masinhvien = d.masinhvien 
+ORDER BY N'Điểm trung bình' DESC
+--1.18
+SELECT masinhvien,hodem+' '+ten as N'Họ và tên',ngaysinh,gioitinh,noisinh
+FROM SINHVIEN
+WHERE noisinh = ( SELECT noisinh
+								FROM SINHVIEN
+								WHERE masinhvien = 'DL03')
+--1.19
+CREATE TABLE [dbo].[SINHVIEN_HO_LE](
+	[masinhvien] [nvarchar](10) NOT NULL,
+	[hodem] [nvarchar](45) NOT NULL,
+	[ten] [nvarchar](15) NOT NULL,
+	[ngaysinh] [date] NOT NULL,
+	[gioitinh] [bit] NOT NULL,
+	[malop] [nvarchar](15) NOT NULL,
+	[noisinh] [nvarchar](250) NULL,
+
+);
+--1.20
+INSERT INTO [dbo].[SINHVIEN_HO_LE] (masinhvien, hodem, ten, ngaysinh, gioitinh, noisinh)
+SELECT masinhvien, hodem, ten, ngaysinh, gioitinh, noisinh
+FROM [dbo].[SINHVIEN]
+WHERE hodem like N'Lê%'
+
+
